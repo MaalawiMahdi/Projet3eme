2d661c9798025d3e757e3f08b2d3b06ff5293973
MaalawiMahdi <mehdi.maalawi@esprit.tn>
2021-02-27 14:19:40 +0100

    gestionUtilisateur inscription update

--- SymfonyApplication/src/Form/UserType.php
+++ SymfonyApplication/src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,11 @@ class UserType extends AbstractType
     {
         $builder
             ->add('mail')
-            ->add('password',PasswordType)
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
 
         ;
     }
