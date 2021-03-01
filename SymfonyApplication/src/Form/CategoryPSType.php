caac94f7e0832666b03914f29e4eb05caa3a0941
ahlemelayeb <ahlem.elayeb@esprit.tn>
2021-03-01 22:41:16 +0100

    CrudProduit

---
+++ SymfonyApplication/src/Form/CategoryPSType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sujet;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SujetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description',TextareaType::class)
+            ->add('lien_image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sujet::class,
+        ]);
+    }
+}
